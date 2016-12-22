9d831c9c827371fa36d431e7c38336e2d200859a
jay <jay.chang@redso.com.hk>
2016-12-22 10:27:51 +0800

    no message

--- signaller/src/main/java/com/jaychang/signaller/core/SignallerModule.java
+++ signaller/src/main/java/com/jaychang/signaller/core/SignallerModule.java
@@ -3,6 +3,6 @@ package com.jaychang.signaller.core;
 import io.realm.annotations.RealmModule;
 
 @RealmModule(library = true, allClasses = true)
-class SignallerModule {
+public class SignallerModule {
 
 }
