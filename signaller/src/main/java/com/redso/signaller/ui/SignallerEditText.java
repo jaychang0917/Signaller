7c2ece7cdef87ef8d5ec0877d1fe81a78bc76d5c
jay <jay.chang@redso.com.hk>
2017-03-07 12:30:29 +0800

    no message

---
+++ signaller/src/main/java/com/redso/signaller/ui/SignallerEditText.java
@@ -0,0 +1,22 @@
+package com.redso.signaller.ui;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.vanniktech.emoji.EmojiEditText;
+
+public class SignallerEditText extends EmojiEditText {
+
+  public SignallerEditText(Context context) {
+    super(context);
+  }
+
+  public SignallerEditText(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public SignallerEditText(Context context, AttributeSet attrs, int defStyle) {
+    super(context, attrs, defStyle);
+  }
+
+}
