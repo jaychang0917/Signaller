51daa64357c2c923e06156fa0161fd43087c2f49
jay <jay.chang@redso.com.hk>
2017-04-13 11:06:55 +0800

    Add close button for photo viewer page

--- signaller/src/main/java/com/jaychang/signaller/ui/PhotoViewerActivity.java
+++ signaller/src/main/java/com/jaychang/signaller/ui/PhotoViewerActivity.java
@@ -1,7 +1,9 @@
 package com.jaychang.signaller.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.jaychang.signaller.R;
@@ -10,6 +12,7 @@ import com.jaychang.utils.AppUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import uk.co.senab.photoview.PhotoView;
 import uk.co.senab.photoview.PhotoViewAttacher;
 
@@ -17,6 +20,8 @@ public class PhotoViewerActivity extends AppCompatActivity {
 
   @BindView(R2.id.photoView)
   PhotoView photoView;
+  @BindView(R2.id.closeView)
+  ImageView closeView;
 
   public static final String EXTRA_IMAGE_URL = "EXTRA_IMAGE_URL";
   private String imageUrl;
@@ -41,6 +46,12 @@ public class PhotoViewerActivity extends AppCompatActivity {
 
     PhotoViewAttacher attacher = new PhotoViewAttacher(photoView);
     attacher.update();
+
+  }
+
+  @OnClick(R2.id.closeView)
+  void close() {
+    finish();
   }
 
 }
